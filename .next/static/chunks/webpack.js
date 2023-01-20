dbe26022580694d01ce7b7ff3b2240c04f08fe39
unknown <thomasbarenghi@gmail.com>
2023-01-20 14:02:43 -0300

    ok

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cad8a961588b2ca8"; }
+/******/ 		__webpack_require__.h = function() { return "f514465cda2ab5a0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
