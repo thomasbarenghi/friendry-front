e12b513f990f9c24dd39102e6e4997927d52ed5d
unknown <thomasbarenghi@gmail.com>
2023-01-20 22:13:16 -0300

    Ok

--- components/alumnos.js
+++ components/alumnos.js
@@ -3,13 +3,15 @@ import React from 'react';
 
 const Post = ({ post }) => {
   return (
-    <div className="d-flex item-t1-inner" id="componente" style={{ gap: 16 }}>
-    <img
+
+
+    <div className="d-flex item-t1-inner" id="componente" style={{ gap: 16 }}> <a href={`Https://${post.attributes.urlLinkedin}`}>
+     <img
       className="imagen-redonda"
       src="../assets/img/11062b_d603be77b66b4a91bccb9d62892b4a7a_mv2.jpg"
       style={{ width: 60, height: 60 }}
-    />
-    <div className="d-flex flex-column justify-content-center">
+     />
+     <div className="d-flex flex-column justify-content-center">
       <h1
         style={{ fontWeight: 400 }}
         className="subtitulo-regular color-neutral900 margin-b-0"
@@ -19,6 +21,7 @@ const Post = ({ post }) => {
       <p className="body-regular color-body margin-b-0">{post.attributes.urlLinkedin}</p>
       
     </div>
+    </a>
   </div>
   );
 };
